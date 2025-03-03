bb5e2224e98614b8eefc4cf4b5fd1f917e6f5775
pietro <pt99vr@gmail.com>
2025-03-03 14:58:21 +0100

    confirm it works

--- ASSEMBLY/program.s
+++ ASSEMBLY/program.s
@@ -6,8 +6,12 @@ another_test:
 .text
 .globl _start
 _start:
-    la   a3, another_test        # a3 = address of 'another_test'
-    lw   t2, 4(a3)               
+    la   a3, another_test
+    la 	 a2, test_memory
+    lw   t2, 0(a3)               
     addi t2, t2, 1               
-    sw   t2, 0(a3)               
+    sw   t2, 0(a2)
+    lw   t0, 0(a2)
+    addi t0, t0, 10
+    sw t0, 8(a3)
     nop             
--- script.sh
+++ script.sh
@@ -3,8 +3,6 @@ riscv32-unknown-elf-as -march=rv32i program.s -o program.o
 riscv32-unknown-elf-ld -T linker.ld program.o -o program.elf
 riscv32-unknown-elf-objcopy -O verilog -j .text program.elf imem.hex  # For instructions
 riscv32-unknown-elf-objcopy -O verilog -j .data program.elf dmem.hex  # For data
-
-
 cd ..
 cp ASSEMBLY/imem.hex Single_Cycle_CPU/
 cp ASSEMBLY/dmem.hex Single_Cycle_CPU/
